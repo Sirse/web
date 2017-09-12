ee2541491d734285510651f955c51fa3a35e0e17
Sirse <sirse.box@gmail.com>
2017-09-12 14:56:59 +0700

    Started making of russian translation...

---
+++ resources/lang/ru/dashboard.php
@@ -0,0 +1,32 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Dashboard Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used on the dashboard page.
+	|
+	*/
+
+	'activity' => 'Активность',
+
+	'selectView' => 'Вид',
+
+	'liveView' => 'Live View',
+
+	'lastActivity' => 'Latest Activity',
+
+	'heatmap' => 'Heatmap',
+
+	'overview' => 'Обзор',
+
+	'hour' => 'Hour',
+
+  'average' => 'Average',
+
+	'weekday' => 'Weekday',
+
+);
---
+++ resources/lang/ru/updateprofile.php
@@ -0,0 +1,38 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Update profile Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used on the update profile model.
+	|
+	*/
+
+	'updateProfile' => 'Ваш профиль', 
+
+	'username' => 'Имя пользователя',
+
+	'language' => 'язык',
+
+	'languageInfo' => 'Нет вашего языка или перевод неточен? Помогите нам с переводом в нашем GitHub репозитории.',
+
+	'password' => 'пароль',
+
+	'newPassword' => 'новый пароль',
+
+	'changePassword' => 'Смена пароля',
+
+	'currentPassword' => 'старый пароль',
+
+	'newPassword' => 'новый пароль',
+
+	'confirmNewPassword' => 'подтвердите новый пароль',
+
+	'cancel' => 'отмена',
+
+	'update' => 'обновить'
+
+);
